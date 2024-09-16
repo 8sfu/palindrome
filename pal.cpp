aeb541e191991818368fe71cae43949cfa28b8fd
U-BSD\489895 <489895@SHS-F5W2NW3.beaverton.k12.or.us>
2024-09-16 13:28:17 -0700

    added pal.cpp

---
+++ pal.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+  cout << "Enter a word or phrase with at most 80 characters." << endl;
+  char input[81];
+  cin.clear();
+  cin.get(input, 81);
+  //cout << endl << input << endl;
+
+  int length = 0;
+  char concatInput[81];
+  int i = 0;
+  while(input[i] != '\0'){
+    if(input[i] > 0 && input[i] < 10){
+      concatInput[length] = input[i];
+      i++;
+      //cout << "char " << concatInput[length] << " added at " << i << endl;
+    }
+    else if(input[i] > 64 && input[i] < 91){
+      concatInput[length] = input[i]+32;
+      i++;
+      //cout << "char " << concatInput[length] << " added at " << i << endl;
+      length++;
+    }
+    else if(input[i] > 96 && input[i] < 123){
+      concatInput[length] = input[i];
+      i++;
+      //cout << "char " << concatInput[length] << " added at " << i << endl;
+      length++;
+    } else {
+      i++;
+    }
+  }
+  //cout << concatInput << endl;
+  char finalInput[length+1];
+  char reverseInput[length+1];
+  for (int i = -1; i < length+1; i++){
+    finalInput[i]=concatInput[i];
+    reverseInput[i]=concatInput[length-1-i];
+    //cout << concatInput[i] << " / " << finalInput[i] << " / " << reverseInput[i] << " / " << i+1 << " / " << length << endl;
+  }
+  //cout << finalInput << '.' << endl;
+  //cout << reverseInput << '.' << endl;
+  if(strcmp(reverseInput,finalInput) == 0){
+    cout << endl << "Palindrome" << endl;
+  } else {
+    cout << endl << "Not a palindrome" << endl;
+  }
+  return 0;
+}
